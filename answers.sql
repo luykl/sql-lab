64108c100d75557bb11c09a6e6bea6826feb6ea1
luykl <leah.logsin@gmail.com>
2021-05-25 11:56:34 -0400

    initial commit

---
+++ answers.sql
@@ -0,0 +1,40 @@
+-- 1
+SELECT * FROM customers;
+-- 2
+SELECT DISTINCT country FROM customers;
+-- 3
+SELECT * FROM customers where customer_id LIKE 'BL%';
+-- 4
+SELECT * FROM orders LIMIT 100;
+-- 5
+SELECT * FROM customers WHERE postal_code= '1010' 
+OR postal_code= '3012' 
+OR postal_code= '12209' 
+OR postal_code= '05023';
+-- 6
+SELECT * FROM orders WHERE ship_region IS NOT NULL;
+-- 7
+SELECT * FROM customers ORDER BY country, city; 
+-- 8
+INSERT INTO customers (customer_id, company_name) 
+VALUES ('ABCDE', 'Grand Circus')
+-- 9
+UPDATE orders SET ship_region = 'EuroZone' 
+WHERE ship_country ='France';
+-- 10
+DELETE FROM order_details WHERE quantity = 1;
+-- 11
+SELECT AVG(quantity) FROM order_details;
+SELECT MAX(quantity) FROM order_details;
+SELECT MIN(quantity) FROM order_details;
+-- 12
+SELECT AVG(quantity) FROM order_details GROUP BY order_id;
+SELECT MAX(quantity) FROM order_details GROUP BY order_id;
+SELECT MIN(quantity) FROM order_details GROUP BY order_id;
+-- 13
+SELECT customer_id FROM orders WHERE order_id = 10290;
+-- 14
+-- 15
+-- 16
+-- 17
+-- 18
